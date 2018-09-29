c4af4d26fc1e2fd9286219d1d159ce34885f2c95
Hannah Matthews <hslmatthews@gmail.com>
2018-09-29 19:25:53 +0100

    test

--- Assignment1/src/LCATest.java
+++ Assignment1/src/LCATest.java
@@ -18,4 +18,17 @@ public class LCATest {
 		assertEquals("Find LCA when tree is empty: ", null, tree.findLCA(1, 2) );
 	}
 
+	@Test
+	public void testTwoNodes(){
+		
+		//test tree structure:
+		//			1
+		//		5
+
+		LCA tree = new LCA();
+		tree.root = new Node(1);
+		tree.root.left = new Node(5);
+
+		assertEquals("Find LCA of tree with only root and one child: ", 1, tree.findLCA(1, 5).data);	
+	}
 }
