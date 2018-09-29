6b229d179a22024accf922a69add7d3fb5f2b249
Hannah Matthews <hslmatthews@gmail.com>
2018-09-29 17:50:59 +0100

    Node

--- Assignment1/src/LCA.java
+++ Assignment1/src/LCA.java
@@ -1,3 +1,14 @@
+class Node  
+{ 
+    int data; 
+    Node left, right; 
+   
+    Node(int item)  
+    { 
+        data = item; 
+        left = right = null; 
+    } 
+} 
 
 public class LCA {
 
