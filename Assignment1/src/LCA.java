32d9ee9b3498dd5a9464d85722bc233744cd849c
Hannah Matthews <hslmatthews@gmail.com>
2018-09-29 18:04:27 +0100

    findLCA

--- Assignment1/src/LCA.java
+++ Assignment1/src/LCA.java
@@ -1,5 +1,5 @@
-class Node  
-{ 
+class Node  {
+	
     int data; 
     Node left, right; 
    
@@ -11,5 +11,31 @@ class Node
 } 
 
 public class LCA {
+	
+	
+	Node findLCA(Node node, int n1, int n2) {
 
+		if (node == null) {
+			return null;
+		}
+
+		if (node.data == n1 || node.data == n2) {
+			return node;
+		}
+
+		Node left = findLCA(node.left, n1, n2);
+		Node right = findLCA(node.right, n1, n2);
+
+		if (left != null && right != null) {
+			return node;
+		}
+
+		if (left != null) {
+			return left;
+		} else {
+			return right;
+		}
+
+	}
+	
 }
