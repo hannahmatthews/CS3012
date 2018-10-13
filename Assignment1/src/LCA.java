efb5ba6b917ddf92ac49f4edf8754ca146c5405e
Hannah Matthews <hslmatthews@gmail.com>
2018-10-13 14:26:16 +0100

    Adding old BST assignment
    Instead of coming up with a brand new solution, I've decided to copy in my old solution from a BST assignment from module: CS2010.
    
    Some of the functions are very likely unnecessary, but I will remove them as I see fit while working through the problem.

--- Assignment1/src/LCA.java
+++ Assignment1/src/LCA.java
@@ -2,7 +2,7 @@
 public class LCA <Key extends Comparable<Key>, Value>{
 
 	Node root;
-	
+
 	class Node {
 		private Key key;           				
 		private Value val;         				
@@ -15,6 +15,267 @@ public class LCA <Key extends Comparable<Key>, Value>{
 			this.N = N;
 		}
 	}
-	
-	
+
+	public boolean isEmpty() { 
+		return size() == 0; 
+	}
+
+	// return number of key-value pairs in BST
+	public int size() { 
+		return size(root); 
+	}
+
+	// return number of key-value pairs in BST rooted at x
+	private int size(Node x) {
+		//BST is empty
+		if (x == null) return 0;
+
+		//N is number of nodes
+		else return x.N;
+	}
+
+	/**
+	 *  Search BST for given key.
+	 *  Does there exist a key-value pair with given key?
+	 *
+	 *  @param key the search key
+	 *  @return true if key is found
+	 */
+	public boolean contains(Key key) {
+		return get(key) != null;
+	}
+
+	/**
+	 *  Search BST for given key.
+	 *  What is the value associated with given key?
+	 *
+	 *  @param key the search key
+	 *  @return value associated with the given key if found, or null if no such key exists.
+	 */
+	public Value get(Key key) { 
+		return get(root, key); 
+	}
+
+	private Value get(Node node, Key key) {
+		if (node == null) return null;
+		int cmp = key.compareTo(node.key);
+		if      (cmp < 0) return get(node.left, key);
+		else if (cmp > 0) return get(node.right, key);
+		else return node.val;
+	}
+
+	/**
+	 *  Insert key-value pair into BST.
+	 *  If key already exists, update with new value.
+	 *
+	 *  @param key the key to insert
+	 *  @param val the value associated with key
+	 */
+	public void put(Key key, Value val) {
+		if (val == null) { 
+			delete(key); 
+			return; 
+		}
+		root = put(root, key, val);
+	}
+
+	private Node put(Node node, Key key, Value val) {
+		if (node == null) return new Node(key, val, 1); //new bst
+		int cmp = key.compareTo(node.key);
+		if      (cmp < 0) node.left  = put(node.left,  key, val);
+		else if (cmp > 0) node.right = put(node.right, key, val);
+		else              node.val   = val; //updating value
+		node.N = 1 + size(node.left) + size(node.right); // value child1 + value child2 + 1
+		return node;
+	}
+
+	/**
+	 * Tree height.
+	 *
+	 * Asymptotic worst-case running time using Theta notation: worst case O(N) which would occur when
+	 * the tree would act as a list, that meaning either all the nodes are to right or all the nods are to the left
+	 * when N is the number of nodes in the BST.
+	 * 
+	 * @return the number of links from the root to the deepest leaf.
+	 *
+	 * Example 1: for an empty tree this should return -1.
+	 * Example 2: for a tree with only one node it should return 0.
+	 * Example 3: for the following tree it should return 2.
+	 *   B
+	 *  / \
+	 * A   C
+	 *      \
+	 *       D
+	 */
+
+	public int height() { 
+		return height(root); 
+	}
+
+
+	private int height(Node x) {
+		if (x == null) {
+			return -1;
+		}
+		else {
+			return 1 + Math.max(height(x.left), height(x.right));
+		}
+	}
+
+	/**
+	 * Median key.
+	 * If the tree has N keys k1 < k2 < k3 < ... < kN, then their median key 
+	 * is the element at position (N+1)/2 (where "/" here is integer division)
+	 *
+	 * @return the median key, or null if the tree is empty.
+	 */
+	public Key median() {
+		if (isEmpty()) return null; //returning null if the bst is empty
+
+		else {
+			int median=(((size(root)+1)/2)-1);   //add 1 to size(root)	
+			//need to change int to key?
+			return intToKey(median);
+		}
+	}
+
+	private Key intToKey(int passedInt) {                       
+		Node node = intToKey(root, passedInt);
+		return node.key;
+	}
+	//find the node with equivalent key given passedInt
+	private Node intToKey(Node node, int passedInt) {     
+		int leftSize = (size(node.left));  
+
+		//check is it in the left or right subtree
+		if (leftSize > passedInt) {
+			return intToKey(node.left,  passedInt); 
+		}
+		else if (leftSize < passedInt) {
+			return intToKey(node.right, passedInt-leftSize-1); 
+		}
+		else {
+			return node; 
+		}
+	} 
+
+
+	/**
+	 * Print all keys of the tree in a sequence, in-order.
+	 * That is, for each node, the keys in the left subtree should appear before the key in the node.
+	 * Also, for each node, the keys in the right subtree should appear before the key in the node.
+	 * For each subtree, its keys should appear within a parenthesis.
+	 *
+	 * Example 1: Empty tree -- output: "()"
+	 * Example 2: Tree containing only "A" -- output: "(()A())"
+	 * Example 3: Tree:
+	 *   B
+	 *  / \
+	 * A   C
+	 *      \
+	 *       D
+	 *
+	 * output: "((()A())B(()C(()D())))"
+	 *
+	 * output of example in the assignment: (((()A(()C()))E((()H(()M()))R()))S(()X()))
+	 *
+	 * @return a String with all keys in the tree, in order, parenthesized.
+	 */
+	public String printKeysInOrder() {
+		String res= "";
+		if (isEmpty()) {
+			return res += "()";
+		}
+		else {
+			return res = printKeysInOrder(root);
+		}
+
+	}
+
+	private String printKeysInOrder(Node node) {
+		String res = "";
+		if (node == null) {
+			return res += "()";
+		}
+		else {
+			return res += ("(" + printKeysInOrder(node.left) + node.key.toString() + printKeysInOrder(node.right) + ")");
+		}
+
+	}
+
+	/**
+	 * Pretty Printing the tree. Each node is on one line.
+	 *
+	 * @return a multi-line string with the pretty ASCII picture of the tree.
+	 */
+	public String prettyPrintKeys() {
+		if(isEmpty()) return "-null\n";
+		return prettyPrint(root,"");
+	}
+
+	private String prettyPrint(Node node, String prefix) {
+		if (node == null) {
+			return (prefix + "-null\n");
+		}
+		else {	
+			return (prefix+"-"+node.key.toString()+"\n" +prettyPrint(node.left,(prefix+" |"))+ prettyPrint(node.right,(prefix+"  ")));
+		}
+
+	}
+
+	/**
+	 * Deletes a key from a tree (if the key is in the tree).
+	 * Note that this method works symmetrically from the Hibbard deletion:
+	 * If the node to be deleted has two child nodes, then it needs to be
+	 * replaced with its predecessor (not its successor) node.
+	 *
+	 * @param key the key to delete
+	 */
+	public void delete(Key key) {
+		root = delete(root, key);	
+	}
+
+	private Node delete (Node node, Key key) {
+		if (node == null) { 
+			return null;
+		}
+		int compare = key.compareTo(node.key);
+
+		if   (compare > 0) {
+			node.right = delete(node.right, key);
+			node.left  = delete(node.left,  key);
+		}
+		else if (compare < 0) {
+			node.left  = delete(node.left,  key);
+		}
+		else {
+			if (node.right == null) {
+				return node.left;
+			}
+			if (node.left  == null) {
+				return node.right;
+			}
+			Node temp = node;
+			node = max(temp.left);                              
+			node.left = deleteMax(temp.left);                 
+			node.right = temp.right; 
+		}
+		node.N = size(node.left) + size(node.right) + 1;
+		return node;
+	}
+
+	private Node deleteMax(Node node) {
+		if (node.right == null) return node.left;
+		node.right = deleteMax(node.right);
+		node.N = size(node.left) + size(node.right) + 1;                                 
+		return node;
+	}
+
+	public Node max(Node node) {
+		if(node.right!=null) {
+			return max(node.right);
+		}
+		return node;
+	}
+
 }
--- Assignment1/src/LCATest.java
+++ Assignment1/src/LCATest.java
@@ -5,8 +5,119 @@ import org.junit.Test;
 public class LCATest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDelete() {
+		LCA<Integer, Integer> LCA = new LCA<Integer, Integer>();
+		LCA.delete(1);
+		assertEquals("Deleting from empty tree", "()", LCA.printKeysInOrder());
+
+		LCA.put(7, 7);   //        _7_
+		LCA.put(8, 8);   //      /     \
+		LCA.put(3, 3);   //    _3_      8
+		LCA.put(1, 1);   //  /     \
+		LCA.put(2, 2);   // 1       6
+		LCA.put(6, 6);   //  \     /
+		LCA.put(4, 4);   //   2   4
+		LCA.put(5, 5);   //        \
+		//         5
+
+		assertEquals("Checking order of constructed tree",
+				"(((()1(()2()))3((()4(()5()))6()))7(()8()))", LCA.printKeysInOrder());
+
+		LCA.delete(9);
+		assertEquals("Deleting non-existent key",
+				"(((()1(()2()))3((()4(()5()))6()))7(()8()))", LCA.printKeysInOrder());
+
+		LCA.delete(8);
+		assertEquals("Deleting leaf", "(((()1(()2()))3((()4(()5()))6()))7())", LCA.printKeysInOrder());
+
+		LCA.delete(6);
+		assertEquals("Deleting node with single child",
+				"(((()1(()2()))3(()4(()5())))7())", LCA.printKeysInOrder());
+
+		LCA.delete(3);
+		assertEquals("Deleting node with two children",
+				"(((()1())2(()4(()5())))7())", LCA.printKeysInOrder());
 	}
 
+	@Test
+	public void testPut() {
+		LCA<Integer, Integer> LCA = new LCA<Integer, Integer>();
+		LCA.put(1, null);
+		LCA.put(10, 1);
+		LCA.put(15,2);
+		LCA.put(15, 15);	 
+
+		assertEquals("Putting nodes", "(()10(()15()))", LCA.printKeysInOrder());
+	}
+
+	@Test
+	public void testGet() {
+		LCA<Integer, Integer> LCA = new LCA<Integer, Integer>();
+		assertEquals("Testing empty", false, LCA.contains(5));
+		LCA.put(1, null);
+		LCA.put(10, 1);
+		LCA.put(5, 9);
+		LCA.put(15,2);
+		LCA.put(9, 15);	 
+
+		assertEquals("Testing left", "9", LCA.get(5).toString());
+		assertEquals("Testing right then right", "2", LCA.get(15).toString());
+		assertEquals("Testing right then left", "15", LCA.get(9).toString());
+		assertEquals("Testing root", "1", LCA.get(10).toString());
+	}
+
+	@Test
+	public void testHeight() {
+
+		LCA<Integer, Integer> LCA = new LCA<Integer, Integer>();
+
+		assertEquals("Testing height", -1, LCA.height());
+
+		LCA.put(7, 7);  
+
+		assertEquals("Testing height", 0, LCA.height());
+
+		LCA.put(8, 8);   
+		LCA.put(3, 3);  
+
+		assertEquals("Testing height", 1, LCA.height());
+
+		LCA.put(1, 1);      
+		LCA.put(2, 2);   
+
+		assertEquals("Testing height", 3, LCA.height());
+
+		LCA.put(6, 6);   
+		LCA.put(4, 4);   
+		LCA.put(5, 5);
+
+		assertEquals("Testing height", 4, LCA.height());
+
+	}
+
+	@Test
+	public void testMedian() {
+		LCA<Integer, Integer> LCA = new LCA<Integer, Integer>();
+		assertEquals("Testing median", null, LCA.median());
+		LCA.put(7, 7); 
+		assertEquals("Testing median", "7", LCA.median().toString());
+		LCA.put(8, 8);   
+		LCA.put(3, 3);  
+		assertEquals("Testing median", "7", LCA.median().toString());
+		LCA.put(1, 1);      
+		LCA.put(2, 2); 
+		assertEquals("Testing median", "3", LCA.median().toString());
+		LCA.put(6, 6);   
+		LCA.put(4, 4);   
+		LCA.put(5, 5);
+		assertEquals("Testing median", "4", LCA.median().toString());
+	}
+	
+	@Test
+	public void testContains() {
+		LCA<Integer, Integer> LCA = new LCA<Integer, Integer>();
+		assertEquals("Testing contains", false, LCA.contains(1));
+		LCA.put(7, 7); 
+		assertEquals("Testing contains", true, LCA.contains(7));
+	}
 }
